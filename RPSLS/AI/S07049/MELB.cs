31e753f2cb05e9f995d841eebaae630c0de3d10c
Unknown <felix@felixsoum.com>
2019-03-05 22:24:42 -0500

    Add French doc

--- RPSLS/AI/S07049/MELB.cs
+++ RPSLS/AI/S07049/MELB.cs
@@ -17,8 +17,12 @@ namespace RPSLS
 
         Move counterMove(Move z)
         {
-            int ah = (int)z - 1;
-            ah %= 5; return (Move)ah;
+
+            int ah = ((int)z - 1);
+            ah %= 5;
+
+
+            return (Move)Math.Abs(ah);
         }
 
         Move[] lol = new Move[3];
@@ -28,12 +32,22 @@ namespace RPSLS
 
             for (int i = 1; i < lol.Length; i++)
                 lol[i - 1] = lol[i];
+
+
             lol[lol.Length - 1] = opponentMove;
+            var z = true;
+            foreach (var item in lol)
+                if (item != Move.Rock) z = false;
+            if (z) { c = 3; return; }
             if (b <= 2) return;
             if (((int)lol[0] + (int)lol[1] + (int)lol[2]) / 3 == (int)lol[0]) c = 1;
             if (lol[2] == (counterMove(lol[1])))
                 ok++;
             if (ok >= 3) { c = 2; rr = (int)opponentMove; }
+
+
+
+
 
         }
         int ok = 0;
@@ -47,9 +61,18 @@ namespace RPSLS
             if (b <= 2)
                 return Move.Scissors;
 
-            if (c == 1)
-                return counterMove(lol[1] - 1);
-            else return counterMove((Move)rr++ + 1);
+            switch (c)
+            {
+                case 1:
+                    return counterMove(lol[0] - 1);
+                case 2:
+                    return counterMove((Move)rr++ + 1);
+                case 3:
+                    return counterMove(lol[1] + 2);
+                default:
+                    return counterMove(lol[1]);
+            }
+
 
         }
 
